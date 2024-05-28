04b9dac03b98ab38cd8feff954c909188a3049c0
erika.bravo <erika.bravo@anconaautopartes.com>
2024-05-28 11:04:08 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Box, Container, Grid} from '@mui/material';
+import Header from './Components/Dashboard/Header';
+import SideBar from './Components/Dashboard/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Box sx={{ display: 'flex', width: '100%' }}>
+    <SideBar />
+    <Box component="main" sx={{ width: 'calc(100% - 260px)', flexGrow: 1, p: { xs: 0, sm: 1, lg: 2 }, marginLeft: '260px' }}>
+      <Container>
+        <Grid item xs={12} md={12} lg={12}>
+          <Header />
+        </Grid>
+        {/* Otros componentes o contenido pueden ir aquí */}
+      </Container>
+    </Box>
+  </Box>
   );
 }
-
 export default App;
